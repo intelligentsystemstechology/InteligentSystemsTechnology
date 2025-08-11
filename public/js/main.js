77693862e817c7f3fa773a661168d55b84b15624
ErickNoir <andressierrapote55@gmail.com>
2025-08-11 17:07:19 -0500

    deshabilitar inspeccion de codigo

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,14 @@ document.addEventListener('DOMContentLoaded', () => {
 function scrollToSection(id) {
   document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
 }
+
+
+  document.addEventListener('contextmenu', event => event.preventDefault());
+  document.onkeydown = function(e) {
+    if (e.keyCode == 123 || // F12
+        (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // Ctrl+Shift+I
+        (e.ctrlKey && e.shiftKey && e.keyCode == 74) || // Ctrl+Shift+J
+        (e.ctrlKey && e.keyCode == 85)) { // Ctrl+U
+      return false;
+    }
+  };
